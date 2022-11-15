b3a03af74f5d65f5c5d84f2e92cc950899182728
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2022-11-15 18:53:25 +0200

    Vasak/parempööre eemaldamine töötab

--- src/kodu5/Kodu5.java
+++ src/kodu5/Kodu5.java
@@ -166,54 +166,187 @@ class Kodu5 {
     }
 
     public static Tipp eemaldaKirje(Tipp juur, String info) { // Peaks kohe algul kontrollima kas tegu on tipuga, mis tuleb eemaldada.
-        int juurX = Integer.parseInt(juur.v.info);
-        int eemaldatav = Integer.parseInt(info);
 
-        if (eemaldatav == juurX) {
-            return null;
-        }
-        return null;
+        Tipp tipp = eemaldaKirjeRek(juur, info);
+        juur = tipp;
+
+        return juur;
     }
 
-    public static Tipp eemaldaKirjeRek(Tipp juur, String info) { // Peaks kohe algul kontrollima kas tegu on tipuga, mis tuleb eemaldada.
-        int vasak = Integer.parseInt(juur.v.info);
-        int parem = Integer.parseInt(juur.p.info);
+    public static Tipp eemaldaKirjeRek(Tipp juur, String info) {
+        int võti = Integer.parseInt(juur.info);
         int eemaldatav = Integer.parseInt(info);
+        Tipp tipp = null;
 
-        if (eemaldatav <= vasak) {
-            if (eemaldatav != vasak) {
-                return eemaldaKirje(juur.v, info);
-            } else {// peame alumised üles tooma.
-                juur.v = null;
-                return null;
-            }
+        if (eemaldatav == võti) {
+            Tipp liidetud = liidaAVLpuud(juur.v, juur.p);
+            /*tipp*/
+            juur = liidetud;
+//            return tipp;
+            System.out.println();
+        } else if (eemaldatav < võti) {
+            tipp = eemaldaKirjeRek(juur.v, info);
+            juur.v = tipp;
 
+        } else {
+            tipp = eemaldaKirjeRek(juur.p, info);
+            juur.p = tipp;
         }
 
-        if (eemaldatav >= parem) {
-            if (eemaldatav != parem) {
-                return eemaldaKirjeRek(juur.p, info);
-            } else {// peame alumised üles tooma.
-                juur.p = null;
-                return null;
-            }
-
+        // Tasakaalustamine
+        if (!kasTasakaalustatudPuu(juur)) {
+            juur = tasakaalustaPuu(juur);
         }
 
-        return null;
+        return juur;
+    }
+
+    public static Tipp[] leiaJaEemaldaVäikseim(Tipp tipp) {
+
+        if (tipp.v == null) { // Leidsime väikseima elemendi.
+
+            return new Tipp[]{tipp};
+        }
+
+        Tipp[] andmed = leiaJaEemaldaVäikseim(tipp.v);
+        Tipp väikseim = andmed[0];
+
+        if (väikseim.x != -1) {
+            if (väikseim.p != null) {
+                tipp.v = väikseim.p; // Kui väikseimal eksisteeris parem haru, siis ühendame selle.
+
+            } else tipp.v = null; // Vastasel juhul võtame väiksema ära
+
+            väikseim.x = -1;
+
+            tipp.x = leiaKõrgus(tipp);
+            return new Tipp[]{väikseim, tipp};
+        }
+
+        tipp.x = leiaKõrgus(tipp);
+        return andmed;
+    }
+
+    public static boolean kasTasakaalustatudPuu(Tipp juur) {
+        if (juur == null) return true;
+        int kõrgusV = 0;
+        int kõrgusP = 0;
+
+        if (juur.p != null) {
+            kõrgusP = juur.p.x;
+        }
+
+        if (juur.v != null) {
+            kõrgusV = juur.v.x;
+        }
+
+        return (Math.abs(kõrgusV - kõrgusP) <= 1);
+    }
+
+    public static Tipp tasakaalustaPuu(Tipp juur) {
+
+        int kõrgusV = 0;
+        int kõrgusP = 0;
+
+        if (juur.p != null) {
+            kõrgusP = juur.p.x;
+        }
+
+        if (juur.v != null) {
+            kõrgusV = juur.v.x;
+        }
+
+        int pööratavaHarud = 0; // Fikseerime, millist pööret tuleb teostada.
+
+        if (kõrgusV > kõrgusP) {
+            pööratavaHarud = (juur.p != null && juur.v != null) ? 0b11 :
+                    (juur.p != null) ? 0b01 :
+                            0b10;
+        }
+
+        if (kõrgusV < kõrgusP) {
+            pööratavaHarud = -((juur.p != null && juur.v != null) ? 0b11 :
+                    (juur.p != null) ? 0b01 :
+                            0b10);
+        }
+
+        switch (pööratavaHarud) {
+            // Vasak haru
+            case (0b10) -> juur = paremPööre(juur);
+            case (0b11) -> {
+
+                if (juur.v.v.x > juur.v.p.x) juur = paremPööre(juur);
+                else juur = vasakparemPööre(juur); // Esimesel võimalusel pp, teisel vpp
+            }
+            case (0b01) -> juur = vasakparemPööre(juur);
+
+            // Parem haru
+            case (-0b01) -> juur = vasakPööre(juur);
+            case (-0b11) -> {
+
+                if (juur.p.p.x > juur.p.v.x) juur = vasakPööre(juur);
+                else juur = paremVasakPööre(juur);
+            }
+            case (-0b10) -> juur = paremVasakPööre(juur);
+        }
+
+        return juur;
+    }
+
+    public static Tipp leiaAlluvKeskJärjestuses(Tipp juur) {
+
+        return juur.p != null ? juur.p :
+                juur.v != null ? juur.v :
+                        null;
     }
 
     public static Tipp liidaAVLpuud(Tipp avl1, Tipp avl2) {
-        return null;
+        if (avl1 == null && avl2 == null) return null;
+        if (avl1 == null) return avl2;
+        if (avl2 == null) return avl1;
+
+        // Kasutame parema haru väiksemat elementi uue juurena.
+        Tipp[] andmed = leiaJaEemaldaVäikseim(avl2);
+        Tipp väikseim = andmed[0];
+        Tipp vanem = andmed.length > 1 ? andmed[1] : null;
+
+        if (vanem != null && !kasTasakaalustatudPuu(vanem)) {
+            Tipp tasakaalustatudVanem = tasakaalustaPuu(vanem);
+            Tipp vaadeldav = avl2;
+            if (vanem.equals(vaadeldav)) {
+                väikseim.p = tasakaalustatudVanem;
+            } else {
+
+                boolean leitud = false;
+                while (!leitud) {
+                    if (vanem.equals(vaadeldav.v)) {
+                        avl2.v = tasakaalustatudVanem;
+
+                    }
+                }
+                väikseim.p = avl2;
+            }
+
+        }
+
+        väikseim.v = avl1;
+        väikseim.x = leiaKõrgus(väikseim);
+        System.out.println();
+        return väikseim;
+    }
+
+    public static int leiaKõrgus(Tipp juur) {
+        return Math.max(juur.v == null ? 0 : kõrgus(juur.v),
+                juur.p == null ? 0 : kõrgus(juur.p)) + 1;
     }
 
     public static void main(String[] args) {
 
 //        Tipp AVLpuu = new Tipp("3")
-//        Tipp tipp = new Tipp("3");
+//        Tipp tipp = new Tipp("2");
 //
-//        lisaKirje(tipp, "2");
 //        lisaKirje(tipp, "1");
+//        lisaKirje(tipp, "3");
 //        lisaKirje(tipp, "4");
 //        Tipp tipp = new Tipp("1");
 //
@@ -221,25 +354,27 @@ class Kodu5 {
 //        tipp = lisaKirje(tipp, "3");
 //        Kuvar.kuvaPuu(tipp);
 
-        Tipp tipp1 = new Tipp("5");
-        tipp1 = lisaKirje(tipp1, "6");
-        tipp1 = lisaKirje(tipp1, "3");
-        tipp1 = lisaKirje(tipp1, "4");
-        tipp1 = lisaKirje(tipp1, "2");
-        tipp1 = lisaKirje(tipp1, "1");
-
-//        Tipp tipp1 = new Tipp("4");
-//        tipp1 = lisaKirje(tipp1, "5");
-//        tipp1 = lisaKirje(tipp1, "1");
-//        tipp1 = lisaKirje(tipp1, "2");
-//        tipp1 = lisaKirje(tipp1, "3");
-
 //        Tipp tipp1 = new Tipp("5");
 //        tipp1 = lisaKirje(tipp1, "6");
+//        tipp1 = lisaKirje(tipp1, "3");
+//        tipp1 = lisaKirje(tipp1, "4");
 //        tipp1 = lisaKirje(tipp1, "2");
 //        tipp1 = lisaKirje(tipp1, "1");
+
+        Tipp tipp1 = new Tipp("2");
+        tipp1 = lisaKirje(tipp1, "1");
+        tipp1 = lisaKirje(tipp1, "3");
+        tipp1 = lisaKirje(tipp1, "4");
+        tipp1 = eemaldaKirje(tipp1, "1");
+
+//        Tipp tipp1 = new Tipp("3");
+//        tipp1 = lisaKirje(tipp1, "1");
+//        tipp1 = lisaKirje(tipp1, "6");
+//        tipp1 = lisaKirje(tipp1, "2");
+//        tipp1 = lisaKirje(tipp1, "5");
+//        tipp1 = lisaKirje(tipp1, "7");
 //        tipp1 = lisaKirje(tipp1, "4");
-//        tipp1 = lisaKirje(tipp1, "3");
+//        tipp1 = eemaldaKirje(tipp1, "6");
 
 
         Kuvar.kuvaPuu(tipp1);
